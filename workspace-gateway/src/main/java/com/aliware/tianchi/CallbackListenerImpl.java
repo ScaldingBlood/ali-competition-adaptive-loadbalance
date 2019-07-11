e7720843a2f06280f789d98a4c5504460f0ef5d0
夜岭 <yeling.cy@antfin.com>
2019-07-11 14:04:30 +0800

    async receive

--- workspace-gateway/src/main/java/com/aliware/tianchi/CallbackListenerImpl.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/CallbackListenerImpl.java
@@ -1,5 +1,8 @@
 package com.aliware.tianchi;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import com.aliware.tianchi.remote.Access;
 import com.aliware.tianchi.remote.Balancer;
 
@@ -15,19 +18,22 @@ import org.apache.dubbo.rpc.listener.CallbackListener;
  */
 public class CallbackListenerImpl implements CallbackListener {
     private Balancer balancer = new Balancer();
+    private ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2);
 
     @Override
     public void receiveServerMsg(String msg) {
-        String[] strs = msg.split(" ");
-        if(Access.maxAvailableThreads.containsKey(strs[0])) {
-            double duration = Double.valueOf(strs[1]);
+        threadPool.submit( () -> {
+            String[] strs = msg.split(" ");
+            if(Access.maxAvailableThreads.containsKey(strs[0])) {
+                double duration = Double.valueOf(strs[1]);
 //            if("out".equals(strs[1]))
 //                Access.providerMap.get(strs[0]).decreaseSize();
 //            else
-            Access.providerMap.get(strs[0]).release(duration);
-            balancer.balance(strs[0], duration);
-        }
-        else
-            Access.maxAvailableThreads.put(strs[0], Integer.valueOf(strs[1]));
+                Access.providerMap.get(strs[0]).release(duration);
+                balancer.balance(strs[0], duration);
+            }
+            else
+                Access.maxAvailableThreads.put(strs[0], Integer.valueOf(strs[1]));
+        });
     }
 }
