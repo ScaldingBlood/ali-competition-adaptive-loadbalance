3cf8462fea699e490e6a2a0c3cc91136c9bc0cdd
夜岭 <yeling.cy@antfin.com>
2019-07-11 13:26:42 +0800

    set param

--- workspace-gateway/src/main/java/com/aliware/tianchi/remote/Balancer.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/remote/Balancer.java
@@ -4,6 +4,8 @@
  */
 package com.aliware.tianchi.remote;
 
+import static com.aliware.tianchi.remote.Status.BATCH_SIZE;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +15,7 @@ import java.util.Map;
  * @version $Id: Balancer.java, v 0.1 2019年07月11日 10:07 yeling.cy Exp $
  */
 public class Balancer {
-    private static final int target = 21;
+    private static final double target = 1024 / BATCH_SIZE;
     private Map<String, Double> durations = new HashMap<>();
     private Map<String, Integer> cnt = new HashMap<>();
 
@@ -22,17 +24,17 @@ public class Balancer {
         cnt.put(p, cnt.getOrDefault(p, 1) - 1);
         int size = Access.providerMap.values().stream().map(Status::getSum).reduce(0, (x, y) -> x + y);
 
-        if (duration == Collections.min(durations.values()) && size <= target) {
+        if (duration == Collections.min(durations.values()) && size < target + 3) {
             if (cnt.get(p) == 0) {
                 Status tmp = Access.providerMap.get(p);
-                tmp.increaseSize(2);
+                tmp.increaseSize(1);
                 cnt.put(p, tmp.getSum());
             }
         }
-        if (duration == Collections.max(durations.values()) && size > target) {
+        if (duration == Collections.max(durations.values()) && size > target - 3) {
             if (cnt.get(p) == 0) {
                 Status tmp = Access.providerMap.get(p);
-                tmp.decreaseSize(2);
+                tmp.decreaseSize(1);
                 cnt.put(p, tmp.getSum());
             }
         }
