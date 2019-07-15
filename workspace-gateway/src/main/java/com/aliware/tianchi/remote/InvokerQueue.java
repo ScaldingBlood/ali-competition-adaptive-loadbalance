9ae642528c6448c939b2c61519dbde57c5e63b4c
yzykkpl <yzykkpl@163.com>
2019-07-15 21:03:46 +0800

    weight

--- workspace-gateway/src/main/java/com/aliware/tianchi/remote/InvokerQueue.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/remote/InvokerQueue.java
@@ -7,7 +7,7 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class InvokerQueue {
-    private String[] providers = new String[] {"medium", "large", "small"};
+    private String[] providers = new String[]{"medium", "large", "small"};
 
     private Map<String, Status> providerMap;
 
@@ -15,9 +15,13 @@ public class InvokerQueue {
 
     private Lock lock = new ReentrantLock();
 
+    private Map<String, Double> weightMap = new HashMap<>();
+
+    private Random random = new Random();
+
     public InvokerQueue() {
         providerMap = new HashMap<>();
-        for(int i = 0; i < providers.length; i++) {
+        for (int i = 0; i < providers.length; i++) {
             Status tmp = new Status(this, providers[i]);
             tmp.init();
             providerMap.put(providers[i], tmp);
@@ -26,21 +30,46 @@ public class InvokerQueue {
         Access.providerMap = providerMap;
     }
 
+//    public void sort() {
+//        if (lock.tryLock()) {
+//            entryList.sort((x, y) -> (int) (x.getValue().getCurDuration() - y.getValue().getCurDuration()));
+//            providers = entryList.stream().map(Map.Entry::getKey).toArray(String[]::new);
+//            lock.unlock();
+//        }
+//    }
+
     public void sort() {
-        if(lock.tryLock()) {
-            entryList.sort((x, y) -> (int) (x.getValue().getCurDuration() - y.getValue().getCurDuration()));
-            providers = entryList.stream().map(Map.Entry::getKey).toArray(String[]::new);
+        providers = entryList.stream().map(Map.Entry::getKey).toArray(String[]::new);
+        int sum = 0;
+        if (lock.tryLock()) {
+            for (Map.Entry<String, Status> entry : entryList) {
+                sum += 1.0 / entry.getValue().getCurDuration();
+            }
+            for (Map.Entry<String, Status> entry : entryList) {
+                double curDuration = entry.getValue().getCurDuration();
+                weightMap.put(entry.getKey(), (1.0 / curDuration) / sum);
+            }
             lock.unlock();
         }
     }
 
+
     public String acquire() {
+        double nextDouble = random.nextDouble();
+        double curDouble = nextDouble;
         String[] p = providers;
-        for(int i = 0; i < p.length; i++) {
+        for (int i = 0; i < p.length; i++) {
+            if (curDouble < weightMap.get(p[i])) {
+                curDouble -= weightMap.get(p[i]);
+                continue;
+            }
             Status s = providerMap.get(p[i]);
-            if(s.getCnt() > 0) {
+            if (s.getCnt() > 0) {
                 s.acquire();
                 return p[i];
+            } else {
+                curDouble = (nextDouble / 100) * (1 - weightMap.get(p[i]));
+                i = 0;
             }
         }
 //        double min = ds[0];
